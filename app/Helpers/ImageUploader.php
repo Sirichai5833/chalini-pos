c4e8dfa8134d98aab864edbaacc52edfab73ad38
Siri58331252 <your-e676051000369@mail.rmutk.ac.th>
2026-01-26 17:54:08 +0700

    แก้หน้า stock check

--- app/Helpers/ImageUploader.php
+++ app/Helpers/ImageUploader.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Helpers;
 
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
@@ -9,7 +8,6 @@ class ImageUploader
 {
     public static function upload($file, $folder = 'uploads')
     {
-        // กัน null
         if (!$file || !method_exists($file, 'getRealPath')) {
             return null;
         }
@@ -18,11 +16,9 @@ class ImageUploader
             $file->getRealPath(),
             [
                 'folder' => $folder,
-                'upload_preset' => config('cloudinary.upload_preset'),
             ]
         );
 
-        // ✅ Cloudinary คืนค่าเป็น array
         return $result['secure_url'] ?? null;
     }
 }
